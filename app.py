1732c323ceb1b70735c83da2ef77c5f9e76dda65
adalidg <gutierreztorricosadalid@gmail.com>
2026-06-11 16:37:15 -0400

    app.py

--- app.py
+++ app.py
@@ -724,4 +724,5 @@ if __name__ == '__main__':
     print("\n🚀 Servidor iniciado en: http://localhost:5000")
     print("="*50 + "\n")
     
-    app.run(debug=True, host='localhost', port=5000, threaded=True)
+port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
